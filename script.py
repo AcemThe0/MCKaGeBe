fbe8e52f42a839fd83f090bf804bf3a5dbcb9bcd
AcemThe0 <52658045+AcemThe0@users.noreply.github.com>
2022-04-02 13:23:26 +0300

    Slight improvements
    -Fixed an error were if the ping was not received the whole script would stop
    -Player UUID's are next to their names now

--- script.py
+++ script.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 from mcstatus import JavaServer
 
+port = 25565
 
-def directCheck(ip, port):
+
+def directCheck(ip):
     server = JavaServer(ip, port)
 
     try:
         status = server.status()
-        ping = server.ping()
+
+        try:
+            ping = server.ping()
+        except:
+            ping = "???"
+
         version = status.version.name
 
         players_online = status.players.online
@@ -15,21 +22,20 @@ def directCheck(ip, port):
         player_list = status.players.sample
         message_of_the_day = status.description
 
-
         print(f"\nPlayers online: {players_online}/{players_max}\nLatency: {ping} ms\nVersion: {version}\n\nMOTD:\n{message_of_the_day}\n")
 
         #I am not proud of this
-        print("Players: ", end="")
+        print("Players:")
         if player_list is not None:
             for player in player_list:
-                print(f"{player.name}", end=" ; ")
+                print(f"{player.name} | {player.id}")
         else:
             print("No players online.")
 
         print("\n")
 
     except:
-        print("Failed to get status (server may be offline, non-existent or you may be banned).\n")
+        print("Failed to get status (connection may have been refused).\n")
 
 
-directCheck(input("\nMinecraft server IP (port not required.): "), 25565)
+directCheck(input("\nMinecraft server IP: "))
